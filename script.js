d371d221f2c9c68f4319ec5fea56c0502eed7b11
yasuo055 <anonymousyasuo@gmail.com>
2024-06-12 15:24:47 +0800

    add script

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 let firstNumber = '';
 let secondNumber = '';
-let operator = '';
+let currentOperation = null
+let shouldResetDisplay = false
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.querySelector('equalsBtn')
+const clearButton = document.querySelector('clearBtn')
+const deleteButton = document.querySelector('deleteBtn')
+const pointButton = document.querySelector('pointBtn')
+const lastOperationDisplay = document.querySelector('lastOperationDisplay')
+const currentOperationDisplay = document.querySelector('currentOperationDisplay')
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return '÷'
+    if (keyboardOperator === '*') return '*'
+    if (keyboardOperator === '-') return '−'
+    if (keyboardOperator === '+') return '+'
+  }
 
 
 function getAddition(firstNumber, secondNumber){
@@ -19,7 +36,20 @@ function getDivide(firstNumber, secondNumber){
     return firstNumber / secondNumber;
 }
 
-function operate(){
-    let result = `${firstNumber} ${operator} ${secondNumber}`
-    return result;
+function operate(operator,firstNumber, secondNumber){
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+    switch(operator){
+        case '+':
+            return getAddition(firstNumber, secondNumber)
+        case '-':
+            return getSubtraction(firstNumber, secondNumber)
+        case '*':
+            return getMultiplication(firstNumber, secondNumber)
+        case '÷':
+            if(b === 0) return null
+            else return getDivide(firstNumber, secondNumber)
+        default:
+            return null
+    }
 }
